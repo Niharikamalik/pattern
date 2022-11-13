54e70ce71a705c4402376c88f83339667fda875b
Niharika Malik <73269292+Niharikamalik@users.noreply.github.com>
2022-11-13 11:21:41 +0530

    Create pattern1.java

---
+++ pattern1.java
@@ -0,0 +1,14 @@
+class Solution {
+
+    void printSquare(int n) {
+        // code here
+        for(int i=0; i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+//https://practice.geeksforgeeks.org/problems/square-pattern/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_1
